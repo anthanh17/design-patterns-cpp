0aa67ac8fbcf5085363e9bc164dba0d4f01cf138
Annt <annguyenthe.work@gmail.com>
2022-09-18 21:27:47 +0700

    add builder

---
+++ creational/builder/builder.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class ElectronicProduct {
+    std::string _phone;
+    std::string _tablet;
+    std::string _screen;
+    std::string _laptop;
+public:
+    ElectronicProduct() = default;
+    ~ElectronicProduct() = default;
+
+    void setPhone (const std::string& phone) {
+        _phone = phone;
+    }
+    void setTablet (const std:;string& tablet) {
+        _tablet = tablet;
+    }
+    void setScreen (const std::string& screen) {
+        _screen = screen;
+    }
+    void setLaptop (const)
+
+};
+
+int main() {
+
+}
